89b8333890c7f74a9700caf5cb8325da641fc545
Radi0actvChickn <kpuebla@imsa.edu>
2014-12-10 15:19:16 -0600

    Update and rename TraitLang_c.java to TraitDecl_c.java

---
+++ src/polyglot/ext/trait/ast/TraitDecl_c.java
@@ -0,0 +1,8 @@
+package polyglot.ext.trait.ast;
+
+public class TraitDecl_c extends Term_c implements ClassDecl {
+    public static final TraitDecl_c instance = new TraitDecl_c();
+
+    protected TraitDecl_c() {
+    }
+}
--- src/polyglot/ext/trait/ast/TraitLang_c.java
+++
@@ -1,8 +0,0 @@
-package polyglot.ext.trait.ast;
-
-public class TraitLang_c extends Term_c implements ClassDecl {
-    public static final TraitLang_c instance = new TraitLang_c();
-
-    protected TraitLang_c() {
-    }
-}
