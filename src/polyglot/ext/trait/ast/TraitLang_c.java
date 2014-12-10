3cebcef84031799ef0eb7810a0ce4b873dfc6483
Radi0actvChickn <kpuebla@imsa.edu>
2014-12-10 14:45:18 -0600

    Create TraitLang_c.java

---
+++ src/polyglot/ext/trait/ast/TraitLang_c.java
@@ -0,0 +1,8 @@
+package polyglot.ext.trait.ast;
+
+public class TraitLang_c extends Term_c implements ClassDecl {
+    public static final TraitLang_c instance = new TraitLang_c();
+
+    protected TraitLang_c() {
+    }
+}
