ece5a030765e96e6d2e6f4c4c788948e4414d577
Kody Puebla <kpuebla@imsa.edu>
2015-02-18 13:25:55 -0600

    Add TraitNodeFactory.java

---
+++ src/polyglot/ext/trait/ast/TraitNodeFactory.java
@@ -0,0 +1,25 @@
+package polyglot.ext.trait.ast;
+
+import java.util.List;
+
+import polyglot.ast.Formal;
+import polyglot.ast.Id;
+import polyglot.ast.MethodDecl;
+import polyglot.ast.NodeFactory;
+import polyglot.ast.TypeNode;
+import polyglot.types.ClassType;
+import polyglot.types.Flags;
+import polyglot.util.Position;
+
+public interface TraitNodeFactory extends NodeFactory {
+
+    RequiredMethod RequiredMethod(Position pos, Flags flags,
+            TypeNode returnType, Id name, List<Formal> formals,
+            List<TypeNode> throwTypes);
+
+    TraitDecl TraitDecl(Position pos, Id name,
+            List<RequiredMethod> requiredMethods,
+            List<MethodDecl> providedMethods);
+
+    UseTrait UseTrait(Position pos, Id name, ClassType host);
+}
